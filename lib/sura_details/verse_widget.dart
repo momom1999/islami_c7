c1aab79aa9184741eff52d0ab71a0a30a8e9bae6
mohamed <mohamedmagdyalsaidd@gmail.com>
2022-12-14 07:35:04 +0200

    commit sura details

--- lib/sura_details/verse_widget.dart
+++ lib/sura_details/verse_widget.dart
@@ -15,7 +15,7 @@ class VersrWidget extends StatelessWidget {
         '$content[$index]',
         textDirection: TextDirection.rtl,
         textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 18),
+        style: TextStyle(fontSize: 20),
       ),
     );
   }
